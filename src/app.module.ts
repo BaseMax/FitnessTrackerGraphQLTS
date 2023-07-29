088ca4825c713833554281a9164ffab2c4a4d5a7
Alireza Naghdipour <alirezanqp@gmail.com>
2023-07-29 12:45:22 +0330

    config graphql and add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
-
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PrismaModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
